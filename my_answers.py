9b7fd0e536ef22028d332e22c388cc23f018bd69
Mayur Andulkar <mayurv.andulkar@gmail.com>
2017-09-23 15:21:39 +0200

    Finished with Time series project

--- my_answers.py
+++ my_answers.py
@@ -6,24 +6,32 @@ from keras.layers import LSTM
 import keras
 
 
-# TODO: fill out the function below that transforms the input series 
+# TODO: fill out the function below that transforms the input series
 # and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_series(series, window_size):
     # containers for input/output pairs
     X = []
     y = []
 
-    # reshape each 
+    # reshape each
     X = np.asarray(X)
-    X.shape = (np.shape(X)[0:2])
-    y = np.asarray(y)
-    y.shape = (len(y),1)
+    X = np.zeros(((len(series) - window_size),window_size))
 
+    for i in range(len(X)):
+        X[i][0:window_size] = series[i:i+window_size]
+
+    y = series[window_size:]
+    y = np.asarray(y)
+    y = np.reshape(y, (len(y),1)) #optional
     return X,y
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
+    model = Sequential([
+        LSTM(5, input_shape=(window_size,1)),
+        Dense(1),
+    ])
+    return model
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
@@ -40,7 +48,7 @@ def window_transform_text(text, window_size, step_size):
 
     return inputs,outputs
 
-# TODO build the required RNN model: 
-# a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
+# TODO build the required RNN model:
+# a single LSTM hidden layer with softmax activation, categorical_crossentropy loss
 def build_part2_RNN(window_size, num_chars):
     pass
